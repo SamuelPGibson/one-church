88f2307e59e171a1a22082dbfd584fdbb96f0fb8
Bernard Legaspi <blegaspi26@gmail.com>
2025-06-27 14:35:21 -0400

    cleaning up code

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,5 @@
 import React from "react";
 
-// function Footer() {
-//     return (
-//         <footer className="bg-indigo-600 text-white py-4 mt-auto">
-//             <div className="container mx-auto text-center text-sm">
-//                 &copy; {new Date().getFullYear()} OneChurch. All rights reserved.
-//             </div>
-//         </footer>
-//     );
-// }
-
-// export default Footer;
-
 function Footer() {
     return (
         <footer className="bg-indigo-600 text-white py-4">
